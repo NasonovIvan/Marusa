864fef4dcbad9ed5f06c06de57f07ac3e067d321
NasonovIvan <61712978+NasonovIvan@users.noreply.github.com>
2020-08-06 16:03:23 +0300

    Add ООП

--- task2.cpp
+++ task2.cpp
@@ -1,59 +1,101 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
-//функция ответа по заданной команде и полю
-string answer(string& command, vector<string>& board){
-	int ia = 0;
-	int i1 = 0;
-	char Ca, C1;
-	Ca = command[0];
-	C1 = command[1];
-	while (Ca != board[ia][0]){
-		//cout << "Ca for ia = " << i << " is " << Ca << endl;
-		ia++;
-	}
-	while (C1 != board[0][i1]){
-		//cout << "Ca for ia = " << i << " is " << Ca << endl;
-		i1++;
-	}
-	if (board[ia][i1] == 'x' || board[ia][i1] == 'X'){
-		return "Попал!";
-	}
-	return "Промах!";
-}
+class Sea_fight{
+public:
+	vector<string> commands; //вектор команд
+	char** board; //двумерный массив char, в котором будет поле
+	vector<string> answers; //вектор ответов (попал или промах) в зависимости от команды
 
-//функция считывания комманд
-vector<string> read_commands(){
-	string s;
-	vector<string> v;
-	for(int i = 0; i < 10; i++){
-		cin >> s;
-		v.push_back(s);
-	}
-	return v;
-}
+	/*Sea_fight(){
+		//конструктор
+		board = new char*[10];
+		for(int i = 0; i < 10; i++){
+			board[i] = new char[10];
+		}
+	}*/
 
-//функция считывания поля
-vector<string> read_board(){
-	//считывает доску с консоли
-	vector<string> board;
-	string s;
-	for (int i = 0; i < 10; i++){
-		getline(cin, s);
-		board.push_back(s);
-	}
-	return board;
-}
+    void read_from_file(){
+    	//функция считывания поля из файла
+    	string s;
+    	char c;
+    	ifstream file("board.txt");
+    	board = new char*[10];
+    	for(int i = 0; i < 10; i++){
+    		board[i] = new char[10];
+    	}
+    	//читаем поле 10 на 10
+    	for(int i = 0; i < 10; i++){
+    		for(int j = 0; j < 10; j++){
+    			file >> c;
+    			if(c != ' '){
+    				board[i][j] = c;
+    			}
+    		}
+    	}
+    	//читаем 10 команд и пихаем их в вектор команд
+    	for(int i = 0; i < 10; i++){
+    		file >> s;
+    		commands.push_back(s);
+    	}
+    }
 
-/*void PrintVec(vector<string>& v){
-	for(auto i : v){
-		cout << i << "\n";
-	}
-}*/
+	/*void Read(){
+		//метод ввода поля с консоли
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				cin >> board[i][j];
+			}
+		}
+	}*/
+
+    void Print(){
+    	//вывод карты лабиринта в консоль
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        //вывод вектора команд в консоль
+        for(auto i : commands){
+        	cout << i << endl;
+        }
+    }
+
+    //функция ответа по заданной команде и полю
+    void answer(){
+    	for(auto command : commands){
+    		int ia = 0;
+    		int i1 = 0;
+    		char Ca, C1;
+    		Ca = command[0];
+    		C1 = command[1];
+    		while (Ca != board[ia][0]){
+    			ia++;
+    		}
+    		while (C1 != board[0][i1]){
+    			i1++;
+    		}
+    		if (board[ia][i1] == 'x' || board[ia][i1] == 'X'){
+    			answers.push_back("Попал!");
+    		}
+    		else answers.push_back("Промах!");
+    	}
+    }
+
+    vector<string> return_answers(){
+    	//функция возвращает вектор ответов
+    	return answers;
+    }
+
+};
 
 /* Функция проверки, существует ли файл */
 bool IfFileExists (const string& name)
@@ -63,37 +105,36 @@ bool IfFileExists (const string& name)
 }
 
 int main(){
+
 	//русские буквы в консоли
 	setlocale(LC_ALL, "Russian");
+	//вектор ответов
+	vector<string> answers;
 	// Номер файла
 	int file_number;
 
 	// Бежим по номерам файла 0, 1, 2 ...
-	for(file_number = 0; ; file_number++){
+	for(file_number = 0; ; file_number++)
+	{
 		// Если очередной файл не существует -- выходим из цикла
-		if(!IfFileExists("file" + to_string(file_number) + ".txt")){
+		if(!IfFileExists("file" + to_string(file_number) + ".txt"))
+		{
 			break;
-		}
+	    }
 	}
 
 	//открываем файл
 	ofstream fout("file" + to_string(file_number) + ".txt");
 
-	vector<string> board;
-	vector<string> commands;
-	vector<string> answers;
-
-	board = read_board();
-	commands = read_commands();
-	//PrintVec(board);
-	for(int i = 0; i < 10; i++){
-		string command = commands[i];
-		string s = answer(command, board);
-		answers.push_back(s);
+	Sea_fight Fight;
+	Fight.read_from_file();
+	//Fight.Print();
+	Fight.answer();
+	answers = Fight.return_answers();
+	for(auto answer : answers){
+		cout << answer << endl;
+		fout << answer << endl;
 	}
-	for(int i = 0; i < 10; i++){
-		cout << answers[i] << "\n";
-		fout << answers[i] << "\n";
-	}
+	fout.close();
 	return 0;
 }
